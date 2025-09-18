ac32b29ebda8fd2007f48560189e41b3526f631a
Romilson Assumpção <romilson@arpolar.com.br>
2025-09-18 03:16:05 +0000

    Ao clicar na seta nçao estou conseguindo acessa a pagina de supervisores

--- src/app/(main)/relatorio/page.tsx
+++ src/app/(main)/relatorio/page.tsx
@@ -58,7 +58,7 @@ const initialCovers: ReportCover[] = [
         type: 'motivational',
         title: 'Motivacional',
         subtitle: '',
-        imageUrl: 'https://images.unsplash.com/photo-1506784983877-45594efa4c85?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        imageUrl: 'https://images.unsplash.com/photo-1522071820081-009f0129c71c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         quote: 'Seja um padrão de qualidade. Algumas pessoas não estão acostumadas a um ambiente onde a excelência é esperada.',
         quoteAuthor: 'Steve Jobs',
         characterImageUrl: 'https://i.ibb.co/yYvyfB9/steve-jobs.png',
@@ -347,3 +347,5 @@ export default function ReportPage() {
         </div>
     );
 }
+
+    
